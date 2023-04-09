36cde084a9e6e712cbd6e74380e56a1c1a7125b2
Aditya Dev <aditya.ganapathy@partnerstack.com>
2023-04-09 17:56:04 -0400

    [main] Converted dynamic PHP to statis HTML pages and added Bootstrap support

--- include/banner.php
+++
@@ -1,3 +0,0 @@
-<div class="banner">
-  <h1>Welcome to CodeWomen!</h1>
-</div>
--- include/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<head>
-  <title>Code Women</title>
-  <link
-    rel="stylesheet"
-    type="text/css"
-    href="../styles/index.css">
-</head>
